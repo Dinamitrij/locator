abff5f839af38881b9de80dfc576d32a67960a01
worldwar2 <worldwar2@div.lv>
2015-10-13 11:30:26 +0300

    Java files

---
+++ app/src/main/java/lv/div/locator/Main.java
@@ -0,0 +1,12 @@
+package lv.div.locator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
