6bdca8c41868105c16e84d3e208dcdfab9aeda9a
Dima <dima@zaharov.info>
2015-11-05 13:10:06 +0200

    configs

--- app/src/main/java/lv/div/locator/Const.java
+++ app/src/main/java/lv/div/locator/Const.java
@@ -12,7 +12,8 @@ public class Const {
     public static final int MAX_DB_RECORD_STRING_SIZE = 254;
     public static final String REPORT_URL_MASK = "http://api.thingspeak.com/update?key=XCFK6A93DMV9FUQ2&field1=%s&field2=%s&field3=%s&field4=%s&field5=%s&field6=%s&field7=%s&field8=%s";
     public static final String HEALTH_CHECK_URL_MASK = "http://log2twit.appspot.com/log?t=rob&m=%s";
-    public static final String CONFIG_DOWNLOAD_URL_MASK = "http://locator-ww2.rhcloud.com/data/config?text=1&device=%s";
+    public static final String CONFIG_DOWNLOAD_URL_MASK = "http://server.com/data/config?text=1&device=%s";
+
     public static final String TIME_24H_FORMAT = "HH:mm:ss";
     public static final String UTF8_ENCODING = "utf-8";
     public static final int HEALTH_CHECK_TIME_MSEC = 3600000; // 1 Hour
