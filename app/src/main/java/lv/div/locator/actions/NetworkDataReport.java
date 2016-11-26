dcceef3abcb3329a403394256eb37c00a8ca1c3c
Dima <dima@zaharov.info>
2016-11-26 23:26:28 +0200

    Telemetry SMS; Code cleanup; MLS Fences (in progress)

---
+++ app/src/main/java/lv/div/locator/actions/NetworkDataReport.java
@@ -0,0 +1,50 @@
+package lv.div.locator.actions;
+
+import android.os.AsyncTask;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import lv.div.locator.Utils;
+import lv.div.locator.utils.FLogger;
+
+public class NetworkDataReport extends AsyncTask<String, Void, Boolean> {
+
+    protected HttpURLConnection urlConnection;
+
+    @Override
+    protected Boolean doInBackground(String... params) {
+        FLogger.getInstance().log(this.getClass(), "doInBackground() called");
+        try {
+            URL url = new URL(params[0]);
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            InputStream in = urlConnection.getInputStream();
+            InputStreamReader isw = new InputStreamReader(in);
+
+            int data = isw.read();
+            while (data != -1) {
+                char current = (char) data;
+                data = isw.read();
+            }
+        } catch (Exception e) {
+            FLogger.getInstance().log(this.getClass(), "doInBackground() cannot send/receive data");
+            FLogger.getInstance().log(this.getClass(), "----> " + e.getMessage());
+            FLogger.getInstance().log(this.getClass(), Utils.stToString(e.getStackTrace()));
+        } finally {
+            try {
+                urlConnection.disconnect();
+            } catch (Exception e) {
+                FLogger.getInstance().log(this.getClass(), "doInBackground() cannot urlConnection.disconnect();");
+                FLogger.getInstance().log(this.getClass(), "----> " + e.getMessage());
+                FLogger.getInstance().log(this.getClass(), Utils.stToString(e.getStackTrace()));
+            }
+        }
+
+
+        return true;
+    }
+}
---
+++ app/src/main/java/lv/div/locator/events/EventMlsFenceRequest.java
@@ -0,0 +1,7 @@
+package lv.div.locator.events;
+
+/**
+ * Created by dima on 22-Nov-16.
+ */
+public class EventMlsFenceRequest {
+}
